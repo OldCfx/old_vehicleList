fbf3ea008ba5c2d5057fb11ca611664e08819f94
Old <rafael.elvira03@gmail.com>
2025-11-09 06:50:06 +0100

    custom image

--- shared/config.lua
+++ shared/config.lua
@@ -2,7 +2,9 @@ Config = {}
 
 Config.webhookURL = ""
 
-
+Config.CustomVehicleImages = {
+    ["blista"] = "https://forum-cfx-re.akamaized.net/original/4X/e/3/2/e32d6c2385e9a4af856e47fdcd733684d2809494.webp",
+}
 
 -- Ajouter la perm dans votre cfg
 -- add_ace group.admin permissions.old_vehicleList allow
